45d304db5d1a6ecc1a21eda1e6019d5d0fd9655e
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-04-21 10:18:37 -0700

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Coffee Shop</h1>
+      <h3>The best coffee sorts available in town. Price is per pound.</h3>
+    </React.Fragment>
+  );
+}
+
+export default Header;
