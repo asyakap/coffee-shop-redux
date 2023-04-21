c1f73284c0225645ecc2c6d1673e9b94953f354c
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-04-21 10:57:36 -0700

    add a banner to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import banner from "./../img/banner.jpg"
 
 function Header() {
   return (
     <React.Fragment>
+      <div className="header-container" style={{ backgroundImage: `url(${banner})` }}> </div>
       <h1>Coffee Shop</h1>
+     
       <h3>The best coffee sorts available in town. Price is per pound.</h3>
     </React.Fragment>
   );
