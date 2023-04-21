cfe276e5ecf22fb2af44910d805f41552f67883a
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-04-21 11:28:48 -0700

    add almost of stock functionality

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -9,7 +9,24 @@ function Coffee(props){
 
   let coffeeDisplay = null;
   if (props.weight <= 0) {
-    coffeeDisplay = <h4>{props.name} is <strong>Out of Stock</strong></h4>
+    coffeeDisplay = 
+    <>
+    <h4>{props.name} is <strong>Out of Stock</strong></h4>
+    <hr></hr>
+    </ >
+  } else if (props.weight < 10 & props.weight > 0) {
+    coffeeDisplay =
+    <>
+    <h3>Name: {props.name}</h3>
+    <h3 style={{color: 'red'}}>Almost out of stock</h3>
+    <li>Origin: {props.origin}</li><br />
+    <li>Roast: {props.roast}</li><br />
+    <li>Price per pound: ${props.price}</li><br />
+    <li>Quantity Available: {props.weight}lb.</li>
+    <br />
+    <button className="btn btn-block btn-lg btn-dark" onClick={handleClick}>Buy</button>
+    <hr></hr>
+    </>
   } else {
     coffeeDisplay =
     <>
@@ -18,6 +35,7 @@ function Coffee(props){
     <li>Roast: {props.roast}</li><br />
     <li>Price per pound: ${props.price}</li><br />
     <li>Quantity Available: {props.weight}lb.</li>
+    <br />
     <button className="btn btn-block btn-lg btn-dark" onClick={handleClick}>Buy</button>
     <hr></hr>
     </>
