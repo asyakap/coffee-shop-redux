55a0cc8f884c7ef59f571b420add94fd770f56a3
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-04-25 13:55:58 -0700

    pass the test for DELETE_COFFEE action

--- src/_tests_/reducers/coffee-list-reducer.test.js
+++ src/_tests_/reducers/coffee-list-reducer.test.js
@@ -12,6 +12,24 @@ describe('coffeeListReducer', () => {
     id: 1
   };
 
+  const currentState = {
+    1: {
+      name: 'Coffee1',
+      price: 5,
+      weight: 130,
+      origin: 'Brazil',
+      roast: 'light',
+      id: 1
+    }, 2: {
+      name: 'Coffee2',
+      price: 3,
+      weight: 130,
+      origin: 'Maui',
+      roast: 'dark',
+      id: 2
+    }
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(coffeeListReducer({}, { type: null })).toEqual({});
   });
@@ -36,6 +54,23 @@ describe('coffeeListReducer', () => {
         origin: origin,
         roast: roast,
         id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a coffee', () => {
+    action = {
+      type: 'DELETE_COFFEE',
+      id: 1
+    };
+    expect(coffeeListReducer(currentState, action)).toEqual({
+      2: {
+        name: 'Coffee2',
+        price: 3,
+        weight: 130,
+        origin: 'Maui',
+        roast: 'dark',
+        id: 2
       }
     });
   });
